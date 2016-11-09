08ce85fe660b854e56663cd3c59b4827587853ed
liuyuhua <liuyuhua@yiche.com>
2016-11-09 17:13:15 +0800

    add file

---
+++ App.Angularjs2/wwwroot/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="js/app/app.component.js" />
+/// <reference path="js/app/app.module.js" />
+/// <reference path="js/app/main.js" />
+/// <reference path="js/site.js" />
+/// <reference path="js/systemjs.config.js" />
